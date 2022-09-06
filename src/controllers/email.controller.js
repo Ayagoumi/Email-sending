f0eeca49e20335a822e6c831edf59354399eff63
Ayoub Agoumi <agoumi94@gmail.com>
2022-09-06 08:41:38 +0100

    Update email.controller.js

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -4,7 +4,7 @@ const sendFormEmail = async (req, res) => {
   const data = req.body;
 
   const text = `Name: ${data.name}\nEmail: ${data.email}\nMessage: ${data.message}`;
-  await sendEmail('mozaboub@gmail.com', 'New Form Submission', text);
+  await sendEmail('agoumi94@icloud.com', 'New Form Submission', text);
   res.status(200).send('Email sent successfully');
 };
 
